98c417f459853dc5acaf1655717eeeb8a38a2c41
JosueGamer7772 <josue900333333@gmail.com>
2024-04-15 19:45:45 -0500

    1.4

---
+++ controllers/Gastos-update-controller-Josue.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+require_once( "../models/models_admin.php");
+
+class DBOperations extends DBConfig {
+
+	function dbOperaciones($sql){
+		$this->config();
+		$this->conexion(); 
+		  
+  		$records = $this->Operaciones($sql);		 		  		  		  
+
+  		$this->close();		
+		return $records;				
+	}
+}
+
+/**
+* IMPLEMENTACION DE ACCESO A CONSULTAS PARA PROTEGER MAS LA VISTA
+*/
+class GastosUpdateController extends DBOperations
+{
+	
+	function updateGastos($data){
+		//$hash = password_hash($contra, PASSWORD_DEFAULT);
+        $ejecucion = $this->dbOperaciones("
+				UPDATE gastos 
+                SET fecha='".$data["fch"]."', 
+				concepto='".$data["con"]."', 
+				id_cuentaorigen=".$data["idco"].", 
+				id_clientedestino=".$data["idcd"].", 
+				id_categoria=".$data["idc"]."
+				valor=".$data["val"]."
+                WHERE cod=".$data["cod"]."
+                ");
+		return $ejecucion;												   		
+	}
+	
+}//fin CLASE
+
+
+
+?>

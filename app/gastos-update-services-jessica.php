facaeb3e56ff4d515a21b3523017051ce04a2e95
JessiMeza <jessi151504@gmail.com>
2024-04-15 19:43:41 -0500

    1.4

---
+++ app/gastos-update-services-jessica.php
@@ -0,0 +1,33 @@
+<?php 
+    include "../controllers/gastos-update-controller-jessica.php";    
+
+    class gastosUpdateServices{    
+
+        function updategastos($datos){
+            include "../config/config.php";
+            if(isset($datos["idc"])){//verificar la existencia de envio de datos
+                $objDB = new gastosUpdateController();
+
+                $data = array(
+                    "fch"=> $datos["fch"],
+                    "con"=> $datos["con"],
+                    "idco"=> $datos["idco"],
+                    "idcd"=> $datos["idcd"],
+                    "idc"=> $datos["idc"],
+                    "val"=> $datos["val"]
+                );
+
+                $ejecucion = $objDB->updategastos($data);
+                if($ejecucion){ // Todo se ejecuto correctamente
+                    echo json_encode(array("data"=>null, "error"=>"0", "msg"=>$errorResponse[0] ));                    
+                }else{ // Algo paso mal
+                    echo json_encode(array("data"=>null, "error"=>"10", "msg"=>$errorResponse[10] ));
+                }
+            }else{
+                echo json_encode(array("data"=>null, "error"=>"5", "msg"=>$errorResponse[5] ));
+            }
+        }
+
+    }
+
+?>
